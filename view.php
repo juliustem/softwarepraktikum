218f753f1ca32b3de1b8ed1510381c4906200767
evarom <eva.romanovsky@rambler.ru>
2017-04-10 13:49:51 +0200

    Add files via upload

--- view.php
+++ view.php
@@ -58,6 +58,13 @@ form
     transition: 0.3s;
 }
 
+#myImg6 {
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+
+
 
 
 #myImg:hover {opacity: 0.7;}
@@ -65,6 +72,7 @@ form
 #myImg3:hover {opacity: 0.7;}
 #myImg4:hover {opacity: 0.7;}
 #myImg5:hover {opacity: 0.7;}
+#myImg6:hover {opacity: 0.7;}
 
 
 /* The Modal (background) */
@@ -180,6 +188,10 @@ if( glob($o."hist.png")){
    if( glob($o."heatpearson.png")){
   echo("<img id='myImg5' src='$o/heatpearson.png' alt='Heatmap Pearson' width='300' height='300'>"); 
   }
+   if( glob($o."Rohcluster.png")){
+  echo("<img id='myImg6' src='$o/Rohcluster.png' alt='Clusterdiagramm (Rohdaten)' width='300' height='300'>"); 
+  }
+  
 
 
   
@@ -304,6 +316,31 @@ var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg5');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+</script>
+
+<!-- Für Clusterdiagramm -->
+ <script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg6');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
